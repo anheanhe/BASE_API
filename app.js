a1768bf082572f5b1137d44d9f4529546fbad2c3
Andrea Delgado Herrera <delgadoherreraandrea@Andrea-Andrea-Viviana-Delgado-C02FV3SPML7H.local>
2023-03-13 10:24:22 -0300

    Conexion al servidor https

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
 const express = require('express');
 const conectarDB = require('./config/mongo');
 require('dotenv').config(); // Cargar variables de entorno desde .env
+const http = require('http');
+
 
 const app = express();
 
 // Conexión a la base de datos
 conectarDB();
 
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hola mundo desde Node.js!');
+});
+
+server.listen(3000, 'localhost', () => {
+  console.log('Servidor web escuchando en http://localhost:3000/');
+});
+
 const nuevoUsuario = {
   name: 'Juan',
   email: 'juan@example.com'
 };
 
-fetch('/client', {
-  method: 'POST',
+fetch('/clients', {
   headers: {
     'Content-Type': 'application/json'
   },
